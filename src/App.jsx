483e0a97beedb072bd532f6f74fddf3e58aec22b
Orsouene <orsoueneelaouizeb@gmail.com>
2025-02-07 11:13:43 +0100

    Added Footer(2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./App.css";
-
+import Section from "./components/Footer";
 function App() {
   return (
     <>
       <p className="text-3xl font-bold underline text-pink-600">
         Tailwind bella
       </p>
+      <Section />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Section() {
+  return (
+    <div>
+      <h1 className="text-cyan-700">Prova section</h1>
+    </div>
+  );
+}
+
+export default Section;
