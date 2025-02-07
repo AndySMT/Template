edd52c586f19f8ffb29884ec4b1ed1d96803b07f
Andy Simota <andyiulian.simota@gmail.com>
2025-02-07 11:08:56 +0100

    created folder pages, component whit header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="grid grid-cols-3">
+      <ul>
+        <li>ciao</li>
+        <li>sono</li>
+        <li>io</li>
+        <li>aiaiaia</li>
+      </ul>
+    </div>
+  );
+}
